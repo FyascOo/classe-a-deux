8a671011d50df0b5e0d40ba1cb30ded4366a2e14
FyascOo <alan.choufa@gmail.com>
2023-10-17 21:44:42 +0200

    feat: init indicateur

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -3,3 +3,5 @@ export * from './lib/header/header.component';
 export * from './lib/container/container.component';
 
 export * from './lib/input/input.component';
+
+export * from './lib/button/button.component';
--- libs/table-multiplication/src/lib/table-multiplication/table-multiplication.component.ts
+++ libs/table-multiplication/src/lib/table-multiplication/table-multiplication.component.ts
@@ -1,35 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ContainerComponent, InputComponent } from '@classe-a-deux/shared-ui';
-import { BehaviorSubject, Subject, combineLatest, tap } from 'rxjs';
+import {
+  ButtonComponent,
+  ContainerComponent,
+  InputComponent,
+} from '@classe-a-deux/shared-ui';
+import { BehaviorSubject } from 'rxjs';
 import { TABLES } from './table-multiplication.constante';
 
 @Component({
   selector: 'tm-table-multiplication',
   standalone: true,
-  imports: [CommonModule, ContainerComponent, InputComponent],
+  imports: [CommonModule, ContainerComponent, InputComponent, ButtonComponent],
   template: `
-    <ui-container>
+    <ui-container (keyup.enter)="validate()">
       <ng-container *ngFor="let table of tables; let i = index">
         <ng-container *ngIf="i === (count$ | async)">
-          {{ table.question }} {{ result$ | async }}
+          {{ table.question }} {{ result$ | async }} {{ indicateur }}
         </ng-container>
       </ng-container>
       <ui-input (valueChanges)="result$.next($event)"></ui-input>
+      <ui-button (action)="validate()">Valider</ui-button>
     </ui-container>
   `,
 })
 export class TableMultiplicationComponent {
   tables = TABLES;
-  result$ = new Subject<string>();
+  result$ = new BehaviorSubject<string>('');
   count$ = new BehaviorSubject(0);
-  updateCount$ = combineLatest([this.result$, this.count$])
-    .pipe(
-      tap(([result, count]) => {
-        if (TABLES[count].result === +result) {
-          this.count$.next(this.count$.value + 1);
-        }
-      })
-    )
-    .subscribe();
+  indicateur = '';
+
+  validate() {
+    this.indicateur = 'pas ok';
+    if (TABLES[this.count$.value].result === +this.result$.value) {
+      this.indicateur = 'ok';
+      this.count$.next(this.count$.value + 1);
+    }
+  }
 }
