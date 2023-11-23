657fd18852412fa05208c442c8c5fdfa0ff20ae1
FyascOo <alan.choufa@gmail.com>
2023-11-23 11:16:04 +0100

    feat: selection boule

--- libs/boulier-ux/src/lib/boulier-ux.component.ts
+++ libs/boulier-ux/src/lib/boulier-ux.component.ts
@@ -5,16 +5,28 @@ import {
   ContainerComponent,
 } from '@classe-a-deux/shared-ui';
 import { BehaviorSubject } from 'rxjs';
-
+interface Boule {
+  x: number;
+  y: number;
+}
 @Component({
   selector: 'classe-a-deux-boulier-ux',
   standalone: true,
   imports: [CommonModule, ContainerComponent, ButtonCircleComponent],
   template: `
     <ui-container>
-      <div *ngFor="let y of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]" class="flex w-full">
+      <div
+        *ngFor="let y of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
+        class="flex w-full"
+      >
         <ui-button-circle
-          *ngFor="let x of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]"
+          *ngFor="let x of [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]"
+          (action)="selectedBoule.next({x,y})"
+          [ngClass]="
+            isSelected({x,y})
+              ? 'ml-auto'
+              : null
+          "
         ></ui-button-circle>
       </div>
     </ui-container>
@@ -23,12 +35,43 @@ import { BehaviorSubject } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BoulierUXComponent {
-  one$ = new BehaviorSubject(true);
-  two$ = new BehaviorSubject(true);
-  changeOne() {
-    this.one$.next(!this.one$.value);
+  selectedBoules: Boule[] = [];
+  selectedBoule = new BehaviorSubject<Boule | null>(null);
+
+  constructor() {
+    this.selectedBoule.subscribe((boule) => {
+      // Quand le tableau est vide on ajoute la boule séléctionné
+      if (this.selectedBoules.length === 0 && boule !== null) {
+        this.selectedBoules = [boule];
+      } else {
+        if (boule !== null) {
+          // si on se trouve sur la même ligne on modifie la boule séléctionné
+          if (this.selectedBoules.some(({ y }) => boule.y === y)) {
+            this.selectedBoules = this.selectedBoules
+              .map(({ x, y }) =>
+                //si l'index est supérieur on remplace l'index : on ajoute plus de boule à droite
+                boule.x > x && boule.y === y
+                  ? { x: boule.x, y }
+                  : // sinon l'index est inférieur mais on est sur la même ligne alors on prend l'index de la boule - 1 : on déplace toute les boules à gauche
+                  boule.y === y
+                  ? { x: boule.x - 1, y }
+                  : // sinon on renvoie la valeur du tableau sans modification
+                    { x, y }
+              )
+              // on filtre si jamais la 1er boule a été séléctionné : on vide la ligne
+              .filter(({ x }) => x !== 0);
+          } else {
+            // une nouvelle ligne a été séléctioné on l'ajoute
+            this.selectedBoules = [...this.selectedBoules, boule];
+          }
+        }
+      }
+    });
   }
-  changeTwo() {
-    this.two$.next(!this.two$.value);
+
+  isSelected({ x, y }: { x: number; y: number }) {
+    return this.selectedBoules.some(
+      (coordonne) => coordonne.x === x && coordonne.y === y
+    );
   }
 }
