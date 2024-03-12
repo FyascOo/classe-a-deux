c019c3bc9408d8b13781dcc8862476828135ae13
FyascOo <alan.choufa@gmail.com>
2024-03-12 21:43:25 +0100

    refacto: code review Arthur Lannelucq

--- libs/boulier-ux/src/lib/boulier-ux.component.ts
+++ libs/boulier-ux/src/lib/boulier-ux.component.ts
@@ -1,7 +1,23 @@
 import { NgClass, NgFor } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ButtonCircleComponent, ContainerComponent } from '@classe-a-deux/shared-ui';
-import { BehaviorSubject } from 'rxjs';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Pipe,
+  PipeTransform,
+  signal,
+} from '@angular/core';
+import { ContainsPipe } from '@classe-a-deux/shared-services';
+import { ContainerComponent } from '@classe-a-deux/shared-ui';
+
+@Pipe({
+  name: 'redOrBlue',
+  standalone: true,
+})
+export class IsRedPipe implements PipeTransform {
+  transform({ x, y }: Boule) {
+    return (y <= 5 && x <= 5) || (y > 5 && x > 5) ? 'red' : 'blue';
+  }
+}
 interface Boule {
   x: number;
   y: number;
@@ -9,74 +25,68 @@ interface Boule {
 @Component({
   selector: 'classe-a-deux-boulier-ux',
   standalone: true,
-  imports: [NgFor, NgClass, ContainerComponent, ButtonCircleComponent],
+  imports: [NgFor, NgClass, ContainerComponent, ContainsPipe, IsRedPipe],
   template: `
     <ui-container>
       @for (y of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; track $index) {
       <div class="flex w-full">
         @for ( x of [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]; track $index) {
-        <ui-button-circle
-          (action)="selectedBoule.next({x,y})"
-          [style]="{
-            backgroundColor: (y <= 5 && x <= 5) || (y > 5 && x > 5) ? 'bg-red-800' : 'bg-blue-800',
-            hover: (y <= 5 && x <= 5) || (y > 5 && x > 5) ? 'hover:bg-red-800' : 'hover:bg-blue-800',
-            hoverBorder: (y <= 5 && x <= 5) || (y > 5 && x > 5) ? 'hover:border-red-300' : 'hover:border-blue-300',
-            focus: (y <= 5 && x <= 5) || (y > 5 && x > 5) ? 'focus:ring-red-300' : 'focus:ring-blue-300',
-            margin: 'm-1.5',
-            scale: 'scale-150'
-          }"
-          class="z-20"
-          [ngClass]="
-            isSelected({x,y})
-              ? 'ml-auto'
-              : null
-          "></ui-button-circle>
+        <button
+          (click)="select({x,y})"
+          [ngClass]="{x,y} | redOrBlue"
+          class="border border-black focus:ring-4 m-1.5 scale-150 rounded-full p-2.5 z-20"
+          [class.ml-auto]="{x,y} | contains : selectedBoules()"></button>
         }
 
-        <div class="absolute translate-y-3.5 w-[95%] h-1 bg-slate-800 opacity-50 z-10"></div>
+        <div
+          class="absolute translate-y-3.5 w-[95%] h-1 bg-slate-800 opacity-50 z-10"></div>
       </div>
       }
     </ui-container>
   `,
-  styles: [],
+  styles: [
+    `
+      .red {
+        @apply bg-red-800 hover:bg-red-800 hover:border-red-300 focus:ring-red-300;
+      }
+
+      .blue {
+        @apply bg-blue-800 hover:bg-blue-800 hover:border-blue-300 focus:ring-blue-300;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BoulierUXComponent {
-  selectedBoules: Boule[] = [];
-  selectedBoule = new BehaviorSubject<Boule | null>(null);
+  selectedBoules = signal([] as Boule[]);
 
-  constructor() {
-    this.selectedBoule.subscribe(boule => {
-      // Quand le tableau est vide on ajoute la boule séléctionné
-      if (this.selectedBoules.length === 0 && boule !== null) {
-        this.selectedBoules = [boule];
-      } else {
-        if (boule !== null) {
-          // si on se trouve sur la même ligne on modifie la boule séléctionné
-          if (this.selectedBoules.some(({ y }) => boule.y === y)) {
-            this.selectedBoules = this.selectedBoules
-              .map(({ x, y }) =>
-                //si l'index est supérieur on remplace l'index : on ajoute plus de boule à droite
-                boule.x > x && boule.y === y
-                  ? { x: boule.x, y }
-                  : // sinon l'index est inférieur mais on est sur la même ligne alors on prend l'index de la boule - 1 : on déplace toute les boules à gauche
-                  boule.y === y
-                  ? { x: boule.x - 1, y }
-                  : // sinon on renvoie la valeur du tableau sans modification
-                    { x, y }
-              )
-              // on filtre si jamais la 1er boule a été séléctionné : on vide la ligne
-              .filter(({ x }) => x !== 0);
-          } else {
-            // une nouvelle ligne a été séléctioné on l'ajoute
-            this.selectedBoules = [...this.selectedBoules, boule];
-          }
-        }
-      }
+  select(boule: Boule) {
+    const isSelectedBoulesEmpty = this.selectedBoules().length === 0;
+    if (boule === null) return;
+    else if (isSelectedBoulesEmpty) this.selectedBoules.set([boule]);
+    else
+      this.selectedBoules.set(
+        this._computedSelection(this.selectedBoules(), boule)
+      );
+  }
+
+  private _computedSelection(boules: Boule[], boule: Boule) {
+    const isNewLineSelected = !boules.some(({ y }) => boule.y === y);
+    if (isNewLineSelected) return [...boules, boule];
+
+    return boules.map(({ x, y }) => {
+      const isAnoterLine = boule.y !== y;
+      const isGreaterIndex = boule.x > x && boule.y === y;
+
+      if (isAnoterLine) return { x, y };
+      if (isGreaterIndex) return { x: boule.x, y };
+      else return { x: boule.x - 1, y };
     });
   }
 
   isSelected({ x, y }: { x: number; y: number }) {
-    return this.selectedBoules.some(coordonne => coordonne.x === x && coordonne.y === y);
+    return this.selectedBoules().some(
+      coordonne => coordonne.x === x && coordonne.y === y
+    );
   }
 }
